42f47fca0a0216f91fae84eab372fba816acdefc
Bruno Oliveira <brunooliveiradeveloper@MacBook-Pro-de-Bruno.local>
2022-07-19 16:49:37 -0300

    parser

--- reports_generator/lib/reports_generator.ex
+++ reports_generator/lib/reports_generator.ex
@@ -3,15 +3,12 @@ defmodule ReportsGenerator do
   Documentation for `ReportsGenerator`.
   """
   def build(filename) do
-    "reports/#{filename}"
-    |> File.stream!()
-    |> Enum.map(fn line -> parse_line(line) end)
+    filename
+    |> ReportsGenerator.Parser.parse_file()
+    |> Enum.reduce(report_acc(), fn [id, _food_name, price], report ->
+      Map.put(report, id, report[id] + price)
+    end)
   end
 
-  defp parse_line(line) do
-    line
-    |> String.trim()
-    |> String.split(",")
-    |> List.update_at(2, fn elem -> String.to_integer(elem) end)
-  end
+  defp report_acc, do: Enum.into(1..30, %{}, &{Integer.to_string(&1), 0})
 end
